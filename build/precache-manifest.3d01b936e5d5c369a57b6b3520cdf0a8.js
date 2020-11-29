10ecf75c8c9fc94e416cd4ec74b82bcac64b33df
anassohail99 <anas.sohailabs1@gmail.com>
2020-11-29 21:57:57 +0500

    inital commit

---
+++ build/precache-manifest.3d01b936e5d5c369a57b6b3520cdf0a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9be498a3da49e0db2b51f0851508ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6033f513e2d55c9229f6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cc6be1b5e13be395408",
+    "url": "/static/js/2.67d17e30.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.67d17e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6033f513e2d55c9229f6",
+    "url": "/static/js/main.49010269.chunk.js"
+  },
+  {
+    "revision": "67ee5cc7588e1d3674ac",
+    "url": "/static/js/runtime-main.e1380e13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
